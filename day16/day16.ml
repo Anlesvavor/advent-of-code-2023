08e1f73066bd1776132610e0c5e12ff22413055b
Anlesvavor <jaredmaster32@gmail.com>
2024-01-06 23:20:48 -0600

    Day 16 part 2 B) Domains lets goo

--- day16/day16.ml
+++ day16/day16.ml
@@ -72,7 +72,7 @@ module Laser = struct
         print_endline @@ string_of_list row
       )
 
-  let cast (row : int) (col : int) (t : t) =
+  let cast (row : int) (col : int) (orientation : orientation) (t : t) =
     let max_row = List.fold_left (fun acc ((r, _) ,_) -> max acc r) 0 t in
     let max_col = List.fold_left (fun acc ((_, c) ,_) -> max acc c) 0 t in
     let step row col orientation =
@@ -142,7 +142,7 @@ module Laser = struct
         end
     in
     let initial_tile = List.assoc (row,col) t in
-    walk [] [(row,col), East, initial_tile]
+    walk [] [(row,col), orientation, initial_tile]
     |> condense
 
 end
@@ -150,7 +150,7 @@ end
 let main lines =
   let board = Laser.from_lines lines in
   board
-  |> Laser.cast 0 0
+  |> Laser.cast 0 0 Laser.East
   |> List.length
 
 let () =
@@ -159,3 +159,37 @@ let () =
   let () = List.iter (Printf.printf "%s\n") lines in
   let () = print_newline () in
   print_int (main lines)
+
+
+let main_part_2 lines =
+  let board = Laser.from_lines lines in
+  let max_row = List.fold_left (fun acc ((r, _) ,_) -> max acc r) 0 board in
+  let max_col = List.fold_left (fun acc ((_, c) ,_) -> max acc c) 0 board in
+  let north_border = List.init (succ max_col) (fun col -> 0, col, Laser.South) in
+  let south_border = List.init (succ max_col) (fun col -> max_row, col, Laser.North) in
+  let west_border = List.init (succ max_row) (fun row -> row, 0, Laser.East) in
+  let east_border = List.init (succ max_row) (fun row -> row, max_col, Laser.West) in
+  let work border =
+    List.fold_left (fun acc (row, col, orientation) ->
+        let len =
+          Laser.cast row col orientation board
+          |> List.length
+        in
+        if len > acc then len else acc
+      ) 0 border
+  in
+  let d1 = Domain.spawn (fun _ -> work north_border) in
+  let d2 = Domain.spawn (fun _ -> work south_border) in
+  let d3 = Domain.spawn (fun _ -> work west_border) in
+  let d4 = Domain.spawn (fun _ -> work east_border) in
+  [Domain.join d1; Domain.join d2; Domain.join d3; Domain.join d4] |>
+  List.fold_left (fun acc x ->
+      max (x) acc
+    ) 0
+
+let () =
+  let lines = In_channel.input_lines (In_channel.open_text  "day16.txt") in
+  (* let lines = In_channel.input_lines (In_channel.open_text "day16.example.txt") in *)
+  let () = List.iter (Printf.printf "%s\n") lines in
+  let () = print_newline () in
+  print_int (main_part_2 lines)
