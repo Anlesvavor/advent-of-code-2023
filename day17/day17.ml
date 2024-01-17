786030129aac6440c955c3f0c9eac760d6be0b52
Anlesvavor <jaredmaster32@gmail.com>
2024-01-16 22:33:20 -0600

    Dijsktra algo working

--- day17/day17.ml
+++ day17/day17.ml
@@ -135,6 +135,9 @@ module Node = struct
     |> Seq.map (snd)
     |> List.of_seq
 
+  let eq_coord (a : t) (b : t) : bool =
+    a.coord = b.coord
+
 end
 
 module PQueue = struct
@@ -185,8 +188,9 @@ let auto_fold (f : 'a -> 'a -> 'a) (list : 'a list) : 'a =
   | Some a -> a
   | None -> failwith "auto fold failed!!!"
 
+let min_elt f = auto_fold (fun a b -> if (f a) < (f b) then a else b)
+
 module Dijkstra = struct
-  (* type distances = (node * int) list *)
 
   let distance (a : Node.t) (b : Node.t) : int =
     abs (b.weight - a.weight)
@@ -203,36 +207,36 @@ module Dijkstra = struct
   let neighboors_of_node (node : Node.t) (nodes : Node.t list) : ((Node.t * direction) list) =
     let nodes_coords = (nodes |> List.map (fun (n : Node.t) -> n.coord)) in
     let neighboors_coords = neighboors_of_coord node.coord nodes_coords in
-    neighboors_coords
-    |> List.map (fun (coord, dir) ->
-        List.filter_map (fun (n : Node.t) ->
-            if n.coord = coord
-            then Some (n, dir)
-            else None
-          ) nodes
-      )
+    List.filter_map (fun (n : Node.t) ->
+        let* dir = List.assoc_opt n.coord neighboors_coords in
+        Some (n, dir)
+      ) nodes
 
-  let shortest_path (starter_node : Node.t) (nodes : Node.t list) : int =
+  let shortest_path (starter_node : Node.t)  (nodes : Node.t list) =
     let initial_distances = List.map (fun n ->
         if n = starter_node
         then (n, 0)
         else (n, inf)
       ) nodes
     in
-    let queue = PQueue.queue_of_list (fun n -> n.weight) nodes in
+    let queue = PQueue.queue_of_list (fun (n : Node.t) -> n.weight) nodes in
     let initial_visited = [] in
     let rec loop (distances : (Node.t * int) list) =
       function
       | [] -> (distances)
       | (queue : Node.t PQueue.t) ->
-        let min_node_of_dist = auto_fold (fun acc n ->
-            if (snd acc) < (snd n)
-            then acc
-            else n
-          ) distances
+        let min_node, dist =
+          List.map (fun ((n : Node.t), _) ->
+              let (_, d) = List.find (fun (x, _) ->
+                  Node.eq_coord x n
+                ) distances
+              in
+              (n, d)
+            ) queue
+          |> min_elt (snd)
         in
-        let (cur_opt, (queue' : Node.t PQueue.t)) = PQueue.pop_assoc min_node_of_dist queue in
-        let cur = Option.value cur_opt ~default:(failwith "cur_opt was None!") in
+        let (cur_opt, (queue' : Node.t PQueue.t)) = PQueue.pop_assoc min_node queue in
+        let cur = Option.get cur_opt in
         let neighboors_of_cur = neighboors_of_node cur nodes in
         let neighboors_of_cur_still_in_q =
           neighboors_of_cur
@@ -242,10 +246,11 @@ module Dijkstra = struct
         in
         let distances' =
           neighboors_of_cur_still_in_q
-          |> List.fold_left (fun distance_acc (n, dir) ->
+          |> List.fold_left (fun distance_acc ((n : Node.t), dir) ->
               let get_distance node =
                 List.assoc_opt node distances
-                |> Option.value ~default:(failwith "No distance found for node")
+                |> Option.value ~default:(inf)
+                (* |> Option.value ~default:(failwith "No distance found for node") *)
               in
               let alt = n.weight + get_distance cur in
               if alt < get_distance n
@@ -257,259 +262,22 @@ module Dijkstra = struct
     in
     loop initial_distances queue
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
 
-(* module Graph = struct *)
-(*   module NodeSet = Set.Make(Node) *)
-(*   module NeighboorMap = Map.Make(Node) *)
-(*   type neighboorValue = ((int * int) * direction) list *)
-(*   type t = *)
-(*     { nodes : NodeSet.t *)
-(*     ; neighboors :  neighboorValue NeighboorMap.t *)
-(*     } *)
-
-(*   let graph_of_board (nodes : Node.t list) : t = *)
-(*     let max_row = List.fold_left (fun acc (node : Node.t) -> *)
-(*         max acc (fst node.coord)) 0 nodes *)
-(*     in *)
-(*     let max_col = List.fold_left (fun acc (node : Node.t) -> *)
-(*         max acc (snd node.coord)) 0 nodes *)
-(*     in *)
-(*     let neighboors_coords_of_node (node : Node.t) = *)
-(*       let (row, col) = node.coord in *)
-(*       let e = *)
-(*         let* col' = bounded 0 max_col (succ col) in *)
-(*         Some ((row, col'), East) *)
-(*       in *)
-(*       let s = *)
-(*         let* row' = bounded 0 max_row (succ row) in *)
-(*         Some ((row', col), South) *)
-(*       in *)
-(*       let w = *)
-(*         let* col' = bounded 0 max_col (pred col) in *)
-(*         Some ((row, col'), West) *)
-(*       in *)
-(*       let n = *)
-(*         let* row' = bounded 0 max_row (pred row) in *)
-(*         Some ((row', col), North) *)
-(*       in *)
-(*       List.fold_right (cons_opt) [n;e;s;w] [] *)
-(*     in *)
-(*     let neighboors = nodes |> List.fold_left (fun (acc : neighboorValue NeighboorMap.t) node -> *)
-(*         let neighboors = neighboors_coords_of_node node in *)
-(*         NeighboorMap.add node neighboors acc *)
-(*       ) NeighboorMap.empty *)
-(*     in *)
-(*     let nodes = NodeSet.of_list nodes in *)
-(*     { nodes; neighboors } *)
-
-(*   let neighboors_with_dir_of_node (node : Node.t) (t : t) : (Node.t * direction) list = *)
-(*     let coords : ((int * int) * direction) list = NeighboorMap.find node (t.neighboors) in *)
-(*     let nodes : NodeSet.t = t.nodes in *)
-(*     coords *)
-(*     |> List.fold_left (fun acc (coord, direction) -> *)
-(*         (\* Refactor fo not use Linear Search *\) *)
-(*         let found_node = *)
-(*           NodeSet.filter (fun node -> node.coord = coord) nodes *)
-(*           |> NodeSet.choose_opt in *)
-(*         match found_node with *)
-(*         | Some node -> (node, direction) :: acc *)
-(*         | None -> acc *)
-(*       ) [] *)
-
-(*   let last_node (t : t) : Node.t = *)
-(*     let initial = NodeSet.choose (t.nodes) in *)
-(*     NodeSet.fold (fun node acc -> *)
-(*         if node > acc *)
-(*         then node *)
-(*         else acc *)
-(*       ) (t.nodes) initial *)
-
-(* end *)
-
-(* module Pathfinding = struct *)
-(*   let is_reverse a b = *)
-(*     match a, b with *)
-(*     | North, South | South, North -> true *)
-(*     | East, West | West, East -> true *)
-(*     | _, _ -> false *)
-
-(*   let no_consecutive_rule (d : direction) (d0 : direction) (d1 : direction) (d2 : direction) : bool = *)
-(*     match d, d0, d1, d2 with *)
-(*     | a, b, c, f when (a = b) && (b = c) && (c = f) -> false *)
-(*     | a, b, _, _ -> not (is_reverse a b) *)
-
-(*   let is_direction_list_valid (directions : direction list) : bool = *)
-(*     match directions with *)
-(*     | [] -> true *)
-(*     | (hd :: tl) as path -> *)
-(*       let rec aux = *)
-(*         function *)
-(*         | a :: ((b :: c :: d :: _) as tl) -> *)
-(*           if no_consecutive_rule a b c d *)
-(*           then false *)
-(*           else if is_reverse a b *)
-(*           then false *)
-(*           else aux tl *)
-(*         | a :: ((b :: _) as tl) -> *)
-(*           if is_reverse a b *)
-(*           then false *)
-(*           else aux tl *)
-(*         | _ -> true *)
-(*       in *)
-(*       aux path *)
-
-(*   let is_node_valid (node : Node.t) : bool = *)
-(*     is_direction_list_valid @@ Node.directions_of_path node *)
-
-(*   let is_valid_given_direction (node : Node.t) (dir : direction) : bool = *)
-(*     let directions = Node.directions_of_path node in *)
-(*     let directions = dir :: directions in *)
-(*     is_direction_list_valid directions *)
-
-(*   let get_visit_index (graph : Graph.t) : int = *)
-(*     Graph.NodeSet.elements graph.nodes *)
-(*     |> List.map (fun (node : Node.t) -> node.visit_count) *)
-(*     |> function *)
-(*     | hd :: tl ->  List.fold_left (min) hd tl *)
-(*     | [] -> 0 *)
-(*   (\* |> function *\) *)
-(*   (\* | [] -> failwith "?!?!??!?!?!??!" *\) *)
-(*   (\* | [(n : Node.t)] -> n.visit_count *\) *)
-(*   (\* | hd :: tl -> *\) *)
-
-(*   (\*   let node = List.fold_left (fun (acc : Node.t) (node : Node.t) -> *\) *)
-(*   (\*       if acc.visit_count > node.visit_count *\) *)
-(*   (\*       then node *\) *)
-(*   (\*       else acc *\) *)
-(*   (\*     ) hd tl *\) *)
-(*   (\*   in *\) *)
-(*   (\*   node.visit_count *\) *)
-
-(*   let node_with_lowest_score (graph : Graph.t) : Node.t = *)
-(*     let current_visit_index = get_visit_index graph in *)
-(*     Graph.NodeSet.elements graph.nodes *)
-(*     |> List.filter (fun (node : Node.t) -> node.visit_count = current_visit_index) *)
-(*     |> List.filter (fun (node : Node.t) -> is_node_valid node) *)
-(*     |> function *)
-(*     | hd :: tl -> *)
-(*       List.fold_left (fun (acc: Node.t) (node : Node.t) -> *)
-(*           if node.visit_count > current_visit_index *)
-(*           then acc *)
-(*           else if node.score < acc.score *)
-(*           then node *)
-(*           else acc *)
-(*         ) hd tl *)
-(*     | [a] -> a *)
-(*     | [] -> failwith "No nodes?!" *)
-
-(*   let calculate_score (cur_node : Node.t) (next_node : Node.t) : int = *)
-(*     if cur_node.score = inf *)
-(*     then inf *)
-(*     else cur_node.score + next_node.weight *)
-
-
-(*   let algorithm (graph : Graph.t) (start_node : Node.t) (target_node : Node.t) = *)
-(*     let start_node = Graph.NodeSet.find start_node graph.nodes in *)
-(*     let nodes = Graph.NodeSet.map (fun node -> *)
-(*         if node.coord = start_node.coord *)
-(*         then {node with score = 0} *)
-(*         else node *)
-(*       ) graph.nodes *)
-(*     in *)
-(*     let graph = {graph with nodes=nodes } in *)
-(*     let rec loop graph = *)
-(*       let visit_index = get_visit_index graph in *)
-(*       let () = print_endline @@ string_of_int visit_index in *)
-(*       let cur = node_with_lowest_score graph in *)
-(*       let cur = { cur with visit_count = (succ cur.visit_count) } in *)
-(*       let nodes = Graph.NodeSet.map (fun node -> *)
-(*           if node.coord = cur.coord *)
-(*           then cur *)
-(*           else node *)
-(*         ) graph.nodes *)
-(*       in *)
-(*       (\* Use neigh as a function *\) *)
-(*       let neighbors = Graph.NeighboorMap.find cur graph.neighboors in *)
-(*       let nodes = Graph.NodeSet.map (fun node -> *)
-(*           match List.assoc_opt node.coord neighbors with *)
-(*           | Some dir when ((node.visit_count <= visit_index) && (is_valid_given_direction node dir)) -> *)
-(*             let () = print_endline @@ *)
-(*               (string_of_int node.visit_count) ^ " " *)
-(*               ^ (Node.string_of_node node) *)
-(*               ^ (string_of_direction dir) *)
-(*               ^ (Node.string_of_path node) *)
-(*             in *)
-(*             let new_score = calculate_score cur node in *)
-(*             if (new_score < node.score) *)
-(*             then { node with score = new_score; route_to_node = Some (cur, dir) } *)
-(*             else node *)
-(*           | _ -> node *)
-(*         ) nodes *)
-(*       in *)
-(*       let graph = { graph with nodes=nodes } in *)
-(*       if cur.coord = target_node.coord *)
-(*       then Result.Ok cur *)
-(*       else if (node_with_lowest_score graph).score = inf *)
-(*       then loop graph *)
-(*       else loop graph *)
-(*     in *)
-(*     loop graph *)
-
-(* end *)
-
-
-let main lines =
-  let nodes = Node.nodes_from_lines lines in
-  let graph = Graph.graph_of_board nodes in
-  (* let starter_node = NodeSet.find_first (fun elt -> elt.coord = (0, 0)) graph.nodes in *)
-  let starter_node = Graph.NodeSet.min_elt (graph.nodes) in
-  let target_node = Graph.last_node graph in
-  Pathfinding.algorithm graph starter_node target_node
-  |> function
-  | Ok node ->
-    Node.string_of_path node
-  (* |> Seq.map (fun ((node : Node.t), _) -> node.weight) *)
-  (* |> List.of_seq *)
-  (* |> List.rev *)
-  (* |> Seq.fold_left (+) 0 *)
-  | Error e -> e
 
 let main_2 lines =
   let nodes = Node.nodes_from_lines lines in
-  let graph = Graph.graph_of_board nodes in
-  (* let starter_node = NodeSet.find_first (fun elt -> elt.coord = (0, 0)) graph.nodes in *)
-  let starter_node = Graph.NodeSet.min_elt (graph.nodes) in
-  let target_node = Graph.last_node graph in
-  Pathfinding.algorithm graph starter_node target_node
-  |> function
-  | Ok node ->
-    Node.string_of_path node
-  (* |> Seq.map (fun ((node : Node.t), _) -> node.weight) *)
-  (* |> List.of_seq *)
-  (* |> List.rev *)
-  (* |> Seq.fold_left (+) 0 *)
-  | Error e -> e
+  let starter_node = List.find (fun (n : Node.t) -> n.coord = (0, 0)) nodes in
+  Dijkstra.shortest_path starter_node nodes
+;;
 
-let () =
-  (* let lines = In_channel.input_lines (In_channel.open_text  "day17.txt") in *)
-  let lines = In_channel.input_lines (In_channel.open_text "day17.example.txt") in
-  let () = List.iter (Printf.printf "%s\n") lines in
-  let () = print_newline () in
-  print_endline (main lines)
+In_channel.input_lines (In_channel.open_text  "day17.example.txt")
+|> main_2
+(* let () = *)
+(*   (\* let lines = In_channel.input_lines (In_channel.open_text  "day17.txt") in *\) *)
+(*   let lines = In_channel.input_lines (In_channel.open_text "day17.example.txt") in *)
+(*   let () = List.iter (Printf.printf "%s\n") lines in *)
+(*   let () = print_newline () in *)
+(*   print_endline (main lines_2) *)
 (* (main lines) |> List.iter (print_int) *)
 (* print_int @@ main lines *)
